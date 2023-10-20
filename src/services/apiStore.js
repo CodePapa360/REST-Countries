32566fdff6f3cb61b6d3b1e11e3fffd7e736d146
codepapa <codepapa360@gmail.com>
2023-10-20 17:22:45 +0600

    api-bug-fix

--- src/services/apiStore.js
+++ src/services/apiStore.js
@@ -6,5 +6,14 @@ export async function getCountries() {
   if (!res.ok) throw Error("Failed getting countries");
 
   const data = await res.json();
+  const AFG = data.find((cn) => cn.alpha3Code === "AFG");
+
+  // There is a mistake in the Afganistan's flag in the api data. So I replaced it immediately.
+
+  AFG.flags.svg =
+    "https://upload.wikimedia.org/wikipedia/commons/c/cd/Flag_of_Afghanistan_%282013%E2%80%932021%29.svg";
+  AFG.flag =
+    "https://upload.wikimedia.org/wikipedia/commons/c/cd/Flag_of_Afghanistan_%282013%E2%80%932021%29.svg";
+
   return data;
 }
