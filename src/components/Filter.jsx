27261bc834464941ed525f110ffaf8fe411b5569
codepapa <codepapa360@gmail.com>
2023-10-20 14:42:16 +0600

    fix-minor-bug

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -21,7 +21,7 @@ function Filter() {
     >
       <option value="All">Filter by Region</option>
       <option value="Africa">Africa</option>
-      <option value="America">America</option>
+      <option value="Americas">America</option>
       <option value="Asia">Asia</option>
       <option value="Europe">Europe</option>
       <option value="Oceania">Oceania</option>
