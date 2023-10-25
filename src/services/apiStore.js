b45346d299172086f49116e9f42195f180219fa5
codepapa <codepapa360@gmail.com>
2023-10-25 23:43:27 +0600

    fix Afganistan's flag

--- src/services/apiStore.js
+++ src/services/apiStore.js
@@ -4,5 +4,12 @@ export async function getCountries() {
   const res = await fetch(API_URL);
   if (!res.ok) throw Error("Failed getting countries");
   const data = await res.json();
+
+  // There is a mistake in the Afganistan's flag in the api data. So I replaced it immediately.
+
+  const AFG = data.find((cn) => cn.cca3 === "AFG");
+  AFG.flags.svg =
+    "https://upload.wikimedia.org/wikipedia/commons/c/cd/Flag_of_Afghanistan_%282013%E2%80%932021%29.svg";
+
   return data;
 }
